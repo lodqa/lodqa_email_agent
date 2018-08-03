34054d55388162402ef50c2c8390f990e64119ed
shsukh <39178089+shsukh@users.noreply.github.com>
2018-08-03 15:13:42 +0900

    DEV-1 LODQA BSにHTTPリクエストを送る (#2)
    * DEV-1 LODQA BSにHTTPリクエストを送る
    
    * DEV-1 コメント指摘対応（追加済gem位置変更、modelをモジュール化など）

---
+++ app/models/lodqa_client.rb
@@ -0,0 +1,14 @@
+module LodqaClient
+  class << self
+    SERVER_URL = 'http://localhost/queries'
+    CALLBACK_URL = 'https://webhook.site/7857288e-e2a6-4030-bce4-055b312752c9'
+
+    def post_query question
+      post_params = { query: question,
+                      start_search_callback_url: CALLBACK_URL,
+                      finish_search_callback_url: CALLBACK_URL }
+
+      RestClient::Request.execute method: :post, url: SERVER_URL, payload: post_params
+    end
+  end
+end
