6de5fa5338b64a5eabebee2e66028a97707c358a
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-08-03 16:42:53 +0900

    DEV-3 Rubocopを入れる

--- app/models/lodqa_client.rb
+++ app/models/lodqa_client.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
+# LODQA BSにHTTPリクエストを送る
 module LodqaClient
   class << self
     SERVER_URL = 'http://localhost/queries'
     CALLBACK_URL = 'https://webhook.site/7857288e-e2a6-4030-bce4-055b312752c9'
 
-    def post_query question
+    def post_query(question)
       post_params = { query: question,
                       start_search_callback_url: CALLBACK_URL,
                       finish_search_callback_url: CALLBACK_URL }
