af45970b2c480ec11151dd9f8db06d87fa4053bb
shsukh <39178089+shsukh@users.noreply.github.com>
2018-08-06 17:53:40 +0900

    DEV-4 コールバックを受け入れるパスを作る (#8)
    * DEV-4 コールバックを受け入れるパスを作る
    
    * DEV-4 コメント指摘対応（コントローラ名変更、routeファイル修正）

---
+++ app/controllers/progresses_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# コールバックを受け入れるパスを作る
+class ProgressesController < ApplicationController
+  def create
+    p params
+  end
+
+  def destroy
+    p params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :queries, only: [] do
+     resource :progress, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
