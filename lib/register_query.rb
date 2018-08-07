bf4d3474ce094972da29321649d7a2f6bdb23455
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-08-07 13:28:48 +0900

    DEV-9 コメント指摘対応（クラス定義不要 )

--- lib/register_query.rb
+++ lib/register_query.rb
@@ -1,10 +1,4 @@
 # frozen_string_literal: true
 
 # LODQA BSにクエリーを登録するスクリプトを追加する
-class RegisterQuery
-  def self.execute
-    LodqaClient.post_query 'Which genes are associated with Endothelin receptor type B?'
-  end
-end
-
-RegisterQuery.execute
+LodqaClient.post_query 'Which genes are associated with Endothelin receptor type B?'
