ca5dec59b9192fb284fa05a08730ec20a4e4c84b
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-08-08 14:25:34 +0900

    DEV-16 コメント指摘対応（スタイル変更）

--- app/models/lodqa_client.rb
+++ app/models/lodqa_client.rb
@@ -3,8 +3,8 @@
 # LODQA BSにHTTPリクエストを送る
 module LodqaClient
   class << self
-    SERVER_URL = 'http://' + ENV['HOST_LODQA_BS'] + '/queries'
-    CALLBACK_URL = 'http://' + ENV['HOST'] + '/queries/310d4eab-d454-4087-954e-a4b1638c5af2/progress'
+    SERVER_URL = "http://#{ENV['HOST_LODQA_BS']}/queries"
+    CALLBACK_URL = "http://#{ENV['HOST']}/queries/310d4eab-d454-4087-954e-a4b1638c5af2/progress"
 
     def post_query(question)
       post_params = { query: question,
