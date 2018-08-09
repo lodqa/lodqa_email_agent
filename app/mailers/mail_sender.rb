d8001f0a3382faaf59590d67b1076bc90019d788
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-08-09 15:50:44 +0900

    DEV-19 メールを送信する

---
+++ app/mailers/mail_sender.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# using SendGrid's Ruby Library
+# https://github.com/sendgrid/sendgrid-ruby
+require 'sendgrid-ruby'
+
+# メールを送信する
+module MailSender
+  include SendGrid
+
+  def self.send_mail(subject, body)
+    from = Email.new(email: 'lodqa-mailer@lodqa.org')
+    to = Email.new(email: 'sufujj@gmail.com')
+    content = Content.new(type: 'text/plain', value: body)
+    mail = Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    sg.client.mail._('send').post(request_body: mail.to_json)
+  end
+end
