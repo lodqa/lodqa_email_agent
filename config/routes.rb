0ef69362788485cb03a4dd80e9ffd1948695dffc
shsukh <39178089+shsukh@users.noreply.github.com>
2018-08-15 16:22:46 +0900

    DEV-23 LodqaClient.post_queryに送信先メールアドレスを指定するパラメータを追加 (#25)
    * DEV-23 LodqaClient.post_queryに送信先メールアドレスを指定するパラメータを追加
    
    * DEV-23 コメント指摘対応（route修正、パラメータ名変更）
    
    * DEV-23 コメント指摘対応（routesにコメント追加）
    
    * DEV-23 Gemfile.lock更新前の状態に戻す

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :queries, only: [] do
+  # http://www.nslabs.jp/email-address-regular-expression.rhtml
+  VALID_EMAIL_REGEX = /[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*/
+  resources :queries, constraints: { id: VALID_EMAIL_REGEX } , only: [] do
      resource :progress, only: [:create, :destroy]
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
