f9d849599c3c1cec46bdd1d69ad8c61e3c3678d0
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-08-23 17:51:25 +0900

    DEV-35 コメント指摘対応（変数名修正）

--- lib/register_query.rb
+++ lib/register_query.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-received_mail = MailReceiver.receive_mail
-received_mail.each do |mail|
+received_mails = MailReceiver.receive_mail
+received_mails.each do |mail|
   # LODQA BSにクエリーを登録するスクリプトを追加する
   LodqaClient.post_query mail[:body], mail[:email]
 end
