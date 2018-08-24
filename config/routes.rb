20241fa2f47e6872972fa6118ba31efe21670c12
shsukh <39178089+shsukh@users.noreply.github.com>
2018-08-24 14:29:44 +0900

    DEV-39 コールバックのURLをqueries/query-id/配下からmails/mail-address/配下に変更する (#44)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   # http://www.nslabs.jp/email-address-regular-expression.rhtml
   VALID_EMAIL_REGEX = /[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*/
-  resources :queries, constraints: { id: VALID_EMAIL_REGEX } , only: [] do
+  resources :mails, constraints: { id: VALID_EMAIL_REGEX } , only: [] do
      resource :progress, only: [:create, :destroy]
   end
 
