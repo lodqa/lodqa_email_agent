6a1c58b43897fb1fca951123c391666d601b1bbc
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-08-24 14:43:09 +0900

    DEV-45 未読のメールが不在時のバグ対応

--- app/labor/mail_receiver.rb
+++ app/labor/mail_receiver.rb
@@ -17,11 +17,15 @@ module MailReceiver
     imap.select('INBOX')
     # 未読(UNSEEN)のみ取得する
     ids = imap.search(['UNSEEN'])
-    imap.fetch(ids, 'RFC822').map do |m|
-      mail = Mail.new(m.attr['RFC822'])
-      email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
-      body = mail.text_part.decoded
-      { email: email, body: body.chomp }
+    if ids.empty?
+      {}
+    else
+      imap.fetch(ids, 'RFC822').map do |m|
+        mail = Mail.new(m.attr['RFC822'])
+        email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
+        body = mail.text_part.decoded
+        { email: email, body: body.chomp }
+      end
     end
   end
 end
