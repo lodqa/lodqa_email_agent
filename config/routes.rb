8923b9bc377828c5820ea27f77c1d3812f98c9ac
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-08-28 14:09:09 +0900

    DEV-49 DELETE mails/mail-address/progress は不要

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # http://www.nslabs.jp/email-address-regular-expression.rhtml
   VALID_EMAIL_REGEX = /[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*/
   resources :mails, constraints: { id: VALID_EMAIL_REGEX } , only: [] do
-     resource :progress, only: [:create, :destroy]
+     resource :progress, only: [:create]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
