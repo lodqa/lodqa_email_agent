b6797ceaa0ee4609211c66266b554374898f3287
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-08-31 15:17:25 +0900

    DEV-56 メールの受信方法をIMAPからPOPに変更

--- app/labor/mail_receiver.rb
+++ app/labor/mail_receiver.rb
@@ -1,28 +1,39 @@
 # frozen_string_literal: true
 
-require 'net/imap'
 require 'mail'
+require 'net/pop'
 
 # メールを受信する
 module MailReceiver
-  ADDRESS = 'imap.gmail.com'
+  ADDRESS = 'pop.gmail.com'
   USESSL = true
-  PORT = 993
+  PORT = 995
 
   def self.receive_mail
-    # imapの接続とログイン
-    imap = Net::IMAP.new(ADDRESS, PORT, USESSL)
-    imap.login(ENV['GMAIL_USER_NAME'], ENV['GMAIL_PASSWORD'])
-    # メールボックスを選択
-    imap.select('INBOX')
-    # 未読(UNSEEN)のみ取得する
-    ids = imap.search(['UNSEEN'])
-    return [] if ids.empty?
-    imap.fetch(ids, 'RFC822').map do |m|
-      mail = Mail.new(m.attr['RFC822'])
-      email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
-      body = mail.text_part.decoded
-      { email: email, body: body.chomp }
+    # popの接続
+    Mail.defaults do
+      retriever_method :pop3, address: ADDRESS,
+                              port: PORT,
+                              user_name: ENV['GMAIL_USER_NAME'],
+                              password: ENV['GMAIL_PASSWORD'],
+                              enable_ssl: USESSL
     end
+
+    mails = Mail.all
+    mail_body_array = []
+    mail_body_hash = {}
+
+    return [] if mails.empty?
+    mails.each do |mail|
+      # 受信メール内容
+      body = mail.text_part.decoded
+      email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
+      # メールアドレスと本文内容をハッシュ化
+      mail_body_hash = { email: email, body: body.chomp }
+      # ハッシュを配列に追加
+      mail_body_array.push(mail_body_hash)
+    end
+    # メールアドレスと本文内容の配列
+    mail_body_array
   end
 end
