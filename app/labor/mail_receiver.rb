d1ed810e6b2ca221f4166001de45e413b7f01858
shsukh <39178089+shsukh@users.noreply.github.com>
2018-08-31 16:34:03 +0900

    DEV-56 メールの受信方法をIMAPからPOPに変更 (#70)
    * DEV-56 メールの受信方法をIMAPからPOPに変更
    
    * DEV-56 コメント指摘対応（net/pop不要、each→mapで対応）

--- app/labor/mail_receiver.rb
+++ app/labor/mail_receiver.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
-require 'net/imap'
 require 'mail'
 
 # メールを受信する
 module MailReceiver
-  ADDRESS = 'imap.gmail.com'
+  ADDRESS = 'pop.gmail.com'
   USESSL = true
-  PORT = 993
+  PORT = 995
 
   def self.receive_mail
-    # imapの接続とログイン
-    imap = Net::IMAP.new(ADDRESS, PORT, USESSL)
-    imap.login(ENV['GMAIL_USER_NAME'], ENV['GMAIL_PASSWORD'])
-    # メールボックスを選択
-    imap.select('INBOX')
-    # 未読(UNSEEN)のみ取得する
-    ids = imap.search(['UNSEEN'])
-    return [] if ids.empty?
-    imap.fetch(ids, 'RFC822').map do |m|
-      mail = Mail.new(m.attr['RFC822'])
-      email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
+    # popの接続
+    Mail.defaults do
+      retriever_method :pop3, address: ADDRESS,
+                              port: PORT,
+                              user_name: ENV['GMAIL_USER_NAME'],
+                              password: ENV['GMAIL_PASSWORD'],
+                              enable_ssl: USESSL
+    end
+
+    mails = Mail.all
+    return [] if mails.empty?
+
+    mails.map do |mail|
+      # 受信メール内容
       body = mail.text_part.decoded
+      email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
+      # メールアドレスと本文内容をハッシュ化
       { email: email, body: body.chomp }
     end
   end
