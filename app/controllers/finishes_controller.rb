289a27270c92717c01265fc217a3e790c8ad4739
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-09-05 13:18:03 +0900

    DEV-65 メールの本文をJSONにする

--- app/controllers/finishes_controller.rb
+++ app/controllers/finishes_controller.rb
@@ -6,7 +6,8 @@ class FinishesController < ApplicationController
     # 送信先メールアドレスの取得
     to_email = params[:mail_id]
     # メール本文の取得
-    body = params[:answers]
+    param_answers = params[:answers]
+    body = param_answers.blank? ? '' : param_answers.map(&:to_unsafe_h)
     # 終了メールを送信（SMTPサーバ使用）
     FinishMailer.deliver_email('finish mail', to_email, body)
   end
