c6bcd1176aee3532b5ac3133936576743ef1e1c4
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-09-07 15:30:20 +0900

    DEV-86 コメント指摘対応（eventがnilと想定外のパターン追加）

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,7 @@ class EventsController < ApplicationController
   def create
     # イベントの取得
     event = params[:event]
+    return render status: 400 if event.blank?
     # 送信先メールアドレスの取得
     to_email = params[:mail_id]
     # イベントで判別し、開始・終了のメール送信を行う
@@ -18,6 +19,9 @@ class EventsController < ApplicationController
       body = param_answers.blank? ? '' : param_answers.as_json
       # 終了メールを送信（SMTPサーバ使用）
       FinishMailer.deliver_email('finish mail', to_email, body)
+    else
+      logger = Logger.new(STDERR)
+      logger.info('イベント「'"#{event}"'」は想定していないイベントです。')
     end
   end
 end
