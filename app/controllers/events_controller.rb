d2f9ebe86e170bd2e9e217c3c8c424f1655a342d
shsukh <39178089+shsukh@users.noreply.github.com>
2018-09-07 16:57:47 +0900

    DEV-86 LODQA_BSのパラメータ変更対応 (#87)
    * DEV-86 LODQA_BSのパラメータ変更対応
    
    * DEV-86 コメント指摘対応（eventがnilと想定外のパターン追加）
    
    * DEV-86 コメント指摘対応（Loggerインスタンス不要）

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# コールバックを受け入れるパスを作る
+class EventsController < ApplicationController
+  def create
+    # イベントの取得
+    event = params[:event]
+    return render status: 400 if event.blank?
+    # 送信先メールアドレスの取得
+    to_email = params[:mail_id]
+    # イベントで判別し、開始・終了のメール送信を行う
+    case event
+    when 'start' then
+      # 開始メールを送信（SMTPサーバ使用）
+      StartMailer.deliver_email('start mail', to_email)
+    when 'finish' then
+      # メール本文の取得
+      param_answers = params[:answers]
+      body = param_answers.blank? ? '' : param_answers.as_json
+      # 終了メールを送信（SMTPサーバ使用）
+      FinishMailer.deliver_email('finish mail', to_email, body)
+    else
+      logger.info('イベント「'"#{event}"'」は想定していないイベントです。')
+    end
+  end
+end
