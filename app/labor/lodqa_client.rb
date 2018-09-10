249a6e427c21850dfa12e77a7dd2abb56abdabb3
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-09-10 17:04:15 +0900

    DEV-84 コメント指摘対応（メール文面をERBへ、メールの宛先をクエリメールの送信元アドレスで、例外）

--- app/labor/lodqa_client.rb
+++ app/labor/lodqa_client.rb
@@ -11,8 +11,8 @@ module LodqaClient
                       callback_url: callback_url }
 
       RestClient::Request.execute method: :post, url: SERVER_URL, payload: post_params
-    rescue StandardError
-      FailureMailer.deliver_email('failure mail', 'Gateway error: LODQA BS server is unavailable.')
+    rescue Errno::ECONNREFUSED, Net::OpenTimeout, SocketError
+      FailureMailer.deliver_email('failure mail', address_to_send)
     end
   end
 end
--- app/mailers/failure_mailer.rb
+++ app/mailers/failure_mailer.rb
@@ -4,15 +4,11 @@
 class FailureMailer < ActionMailer::Base
   default from: ENV['FROM_EMAIL']
 
-  def self.deliver_email(subject, body)
-    # 送信先メールアドレスの取得
-    to_email = ENV['FROM_EMAIL']
-
-    build_email(subject, to_email, body).deliver_now
+  def self.deliver_email(subject, to_email)
+    build_email(subject, to_email).deliver_now
   end
 
-  def build_email(subject, to_email, body)
-    @body = body
+  def build_email(subject, to_email)
     mail(to: to_email, subject: subject, &:text)
   end
 end
