3c91b1cb7b37515be33c630617bef9c3bfd80e43
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-09-11 17:54:40 +0900

    失敗時は成功ログを出さない

--- lib/check_new_mails.rb
+++ lib/check_new_mails.rb
@@ -5,5 +5,4 @@ received_mails.each do |mail|
   # LODQA BSにクエリーを登録するスクリプトを追加する
   puts "POST query #{mail[:body]} to the LODQA_BS."
   LodqaClient.post_query mail[:body], mail[:email]
-  puts 'POST finish.'
 end
