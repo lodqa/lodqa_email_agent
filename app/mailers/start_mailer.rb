b34dcfa71db65b6ad5a2d4f724703aa8dfecdc7c
shsukh <39178089+shsukh@users.noreply.github.com>
2018-09-11 17:34:22 +0900

    DEV-93 メールの文面修正 (#95)
    * DEV-93 メールの文面修正
    
    * DEV-93 コメント指摘対応（url修正）

--- app/mailers/start_mailer.rb
+++ app/mailers/start_mailer.rb
@@ -4,12 +4,13 @@
 class StartMailer < ActionMailer::Base
   default from: ENV['FROM_EMAIL']
 
-  def self.deliver_email(subject, to_email, query)
-    build_email(subject, to_email, query).deliver_now
+  def self.deliver_email(subject, to_email, query, search_id)
+    build_email(subject, to_email, query, search_id).deliver_now
   end
 
-  def build_email(subject, to_email, query)
+  def build_email(subject, to_email, query, search_id)
     @query = query
+    @search_id = search_id
     mail(to: to_email, subject: subject, &:text)
   end
 end
