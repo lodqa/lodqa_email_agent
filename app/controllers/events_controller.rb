4c8a37b68eac120a46be721cbb2de88244fa81c3
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-09-11 18:31:08 +0900

    DEV-94 メールのタイトルをクエリに変える

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,20 +10,22 @@ class EventsController < ApplicationController
     to_email = params[:mail_id]
     # 検索IDの取得
     search_id = params[:search_id]
+    # クエリーの取得
+    subject = params[:query][0, 130]
     # イベントで判別し、開始・終了のメール送信を行う
     case event
     when 'start' then
       # クエリーの取得
       query = params[:query]
       # 開始メールを送信（SMTPサーバ使用）
-      StartMailer.deliver_email('start mail', to_email, query, search_id)
+      StartMailer.deliver_email(subject, to_email, query, search_id)
     when 'finish' then
       # メール本文の取得
       answers = params[:answers]
       # 回答の項目数取得
       items_count = answers.blank? ? 0 : answers.length
       # 終了メールを送信（SMTPサーバ使用）
-      FinishMailer.deliver_email('finish mail', to_email, items_count, search_id)
+      FinishMailer.deliver_email(subject, to_email, items_count, search_id)
     else
       logger.info('イベント「'"#{event}"'」は想定していないイベントです。')
     end
