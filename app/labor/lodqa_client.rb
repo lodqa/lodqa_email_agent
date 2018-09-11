d4ecd82cd209a9ff46c0fbbf98dd48bb608b7a5e
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-09-11 15:05:36 +0900

    DEV-91 コメント指摘対応（例外不要）

--- app/labor/lodqa_client.rb
+++ app/labor/lodqa_client.rb
@@ -6,14 +6,12 @@ module LodqaClient
     SERVER_URL = "http://#{ENV['HOST_LODQA_BS']}/searches"
 
     def post_query(question, address_to_send)
-      raise RestClient::ExceptionWithResponse if question.blank?
+      return WarningMailer.deliver_email('warning mail', address_to_send) if question.blank?
       callback_url = "http://#{ENV['HOST_LODQA_EMAIL_AGENT']}/mail/#{address_to_send}/events"
       post_params = { query: question,
                       callback_url: callback_url }
 
       RestClient::Request.execute method: :post, url: SERVER_URL, payload: post_params
-    rescue RestClient::ExceptionWithResponse
-      WarningMailer.deliver_email('warning mail', address_to_send)
     rescue Errno::ECONNREFUSED, Net::OpenTimeout, SocketError
       FailureMailer.deliver_email('failure mail', address_to_send)
     end
