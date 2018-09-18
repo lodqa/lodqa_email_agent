4a7c1377894475bc2c3240e951ab7579921b0c05
shsukh <39178089+shsukh@users.noreply.github.com>
2018-09-18 18:37:55 +0900

    DEV-108 メールの本文でクエリのオプションの値を指定する (#110)
    * DEV-108 メールの本文でクエリのオプションの値を指定する
    
    * DEV-108 コメント指摘対応（rubocopのver指定、空改行・不要コメントの削除、ini情報設定）
    
    * DEV-108 コメント指摘対応（取得本文をini形式で指定、変数名変更、キー名変更）
    
    * DEV-108 指摘対応（iniファイル削除、body['global']返す時にrubocop abcsizeひかかったため、別メソッドで）
    
    * DEV-108 コメント指摘対応（メソッドをprivate化、class << self追加）

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # gem 'rack-cors'
 gem 'rest-client'
 
+gem 'inifile'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -32,7 +34,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'rubocop'
+  gem 'rubocop', '~> 0.58.2'
   gem 'dotenv-rails'
 end
 
--- app/labor/lodqa_client.rb
+++ app/labor/lodqa_client.rb
@@ -4,13 +4,15 @@
 module LodqaClient
   class << self
     SERVER_URL = "http://#{ENV['HOST_LODQA_BS']}/searches"
-
-    def post_query(question, address_to_send)
+    def post_query(question, address_to_send, query_option)
       return WarningMailer.deliver_email('warning mail', address_to_send) if question.blank?
       callback_url = "http://#{ENV['HOST_LODQA_EMAIL_AGENT']}/mail/#{address_to_send}/events"
       post_params = { query: question,
+                      read_timeout: query_option['read_timeout'],
+                      sparql_limit: query_option['sparql_limit'],
+                      answer_limit: query_option['answer_limit'],
+                      cache: query_option['cache'],
                       callback_url: callback_url }
-
       RestClient::Request.execute method: :post, url: SERVER_URL, payload: post_params
       puts 'POST succcess.'
     rescue Errno::ECONNREFUSED, Net::OpenTimeout, SocketError
