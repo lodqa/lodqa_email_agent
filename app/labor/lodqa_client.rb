3e484777b8f9fd554b97fa150ef4780120e56833
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-09-19 16:59:50 +0900

    DEV-112 コメント指摘対応（メソッドと変数の名称変更、unlessをifで）

--- app/labor/lodqa_client.rb
+++ app/labor/lodqa_client.rb
@@ -4,11 +4,11 @@
 module LodqaClient
   class << self
     SERVER_URL = "http://#{ENV['HOST_LODQA_BS']}/searches"
-    def post_query(question, address_to_send, query_option)
+    def post_query(question, address_to_send, option)
       return WarningMailer.deliver_email('warning mail', address_to_send) if question.blank?
       callback_url = "http://#{ENV['HOST_LODQA_EMAIL_AGENT']}/mail/#{address_to_send}/events"
       post_params = { query: question, callback_url: callback_url }
-      post_params = post_params_add(post_params, query_option) unless query_option.blank?
+      post_params = populate_options(post_params, option) if option.present?
 
       RestClient::Request.execute method: :post, url: SERVER_URL, payload: post_params
       puts 'POST succcess.'
@@ -19,12 +19,12 @@ module LodqaClient
 
     private
 
-    # query_optionがある場合はpost_paramsに追加する
-    def post_params_add(post_params, query_option)
-      post_params[:read_timeout] = query_option['read_timeout'] unless query_option['read_timeout'].blank?
-      post_params[:sparql_limit] = query_option['sparql_limit'] unless query_option['sparql_limit'].blank?
-      post_params[:answer_limit] = query_option['answer_limit'] unless query_option['answer_limit'].blank?
-      post_params[:cache] = query_option['cache'] unless query_option['cache'].blank?
+    # optionがある場合はpost_paramsに追加する
+    def populate_options(post_params, option)
+      post_params[:read_timeout] = option['read_timeout'] if option['read_timeout'].present?
+      post_params[:sparql_limit] = option['sparql_limit'] if option['sparql_limit'].present?
+      post_params[:answer_limit] = option['answer_limit'] if option['answer_limit'].present?
+      post_params[:cache] = option['cache'] if option['cache'].present?
       post_params
     end
   end
