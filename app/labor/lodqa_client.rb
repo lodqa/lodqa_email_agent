2ca5760f9d1fa029242202ef99a6f61775c0621c
shsukh <39178089+shsukh@users.noreply.github.com>
2018-09-19 17:05:08 +0900

    DEV-112 受信メールの本文が空の場合はlodqa_client.rbにてRestClient::InternalServerError発生している (#115)
    * DEV-112 受信メールの本文が空の場合はlodqa_client.rbにてRestClient::InternalServerErrorが発生している
    
    * DEV-112 コメント指摘対応（本文にあるオプション情報だけBSに登録）
    
    * DEV-112 コメント指摘対応（メソッドと変数の名称変更、unlessをifで）

--- app/labor/lodqa_client.rb
+++ app/labor/lodqa_client.rb
@@ -4,20 +4,28 @@
 module LodqaClient
   class << self
     SERVER_URL = "http://#{ENV['HOST_LODQA_BS']}/searches"
-    def post_query(question, address_to_send, query_option)
+    def post_query(question, address_to_send, option)
       return WarningMailer.deliver_email('warning mail', address_to_send) if question.blank?
       callback_url = "http://#{ENV['HOST_LODQA_EMAIL_AGENT']}/mail/#{address_to_send}/events"
-      post_params = { query: question,
-                      read_timeout: query_option['read_timeout'],
-                      sparql_limit: query_option['sparql_limit'],
-                      answer_limit: query_option['answer_limit'],
-                      cache: query_option['cache'],
-                      callback_url: callback_url }
+      post_params = { query: question, callback_url: callback_url }
+      post_params = populate_options(post_params, option) if option.present?
+
       RestClient::Request.execute method: :post, url: SERVER_URL, payload: post_params
       puts 'POST succcess.'
     rescue Errno::ECONNREFUSED, Net::OpenTimeout, SocketError
       puts 'POST failed.'
       FailureMailer.deliver_email('failure mail', address_to_send)
+    end
+
+    private
+
+    # optionがある場合はpost_paramsに追加する
+    def populate_options(post_params, option)
+      post_params[:read_timeout] = option['read_timeout'] if option['read_timeout'].present?
+      post_params[:sparql_limit] = option['sparql_limit'] if option['sparql_limit'].present?
+      post_params[:answer_limit] = option['answer_limit'] if option['answer_limit'].present?
+      post_params[:cache] = option['cache'] if option['cache'].present?
+      post_params
     end
   end
 end
