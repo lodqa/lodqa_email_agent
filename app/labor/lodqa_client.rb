17dfebd528bda16344e0d4b1f6060bc8b22a5878
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-09-19 16:16:15 +0900

    DEV-112 コメント指摘対応（本文にあるオプション情報だけBSに登録）

--- app/labor/lodqa_client.rb
+++ app/labor/lodqa_client.rb
@@ -7,22 +7,25 @@ module LodqaClient
     def post_query(question, address_to_send, query_option)
       return WarningMailer.deliver_email('warning mail', address_to_send) if question.blank?
       callback_url = "http://#{ENV['HOST_LODQA_EMAIL_AGENT']}/mail/#{address_to_send}/events"
-      post_params =
-        if query_option.empty?
-          { query: question, callback_url: callback_url }
-        else
-          { query: question,
-            read_timeout: query_option['read_timeout'],
-            sparql_limit: query_option['sparql_limit'],
-            answer_limit: query_option['answer_limit'],
-            cache: query_option['cache'],
-            callback_url: callback_url }
-        end
+      post_params = { query: question, callback_url: callback_url }
+      post_params = post_params_add(post_params, query_option) unless query_option.blank?
+
       RestClient::Request.execute method: :post, url: SERVER_URL, payload: post_params
       puts 'POST succcess.'
     rescue Errno::ECONNREFUSED, Net::OpenTimeout, SocketError
       puts 'POST failed.'
       FailureMailer.deliver_email('failure mail', address_to_send)
+    end
+
+    private
+
+    # query_optionがある場合はpost_paramsに追加する
+    def post_params_add(post_params, query_option)
+      post_params[:read_timeout] = query_option['read_timeout'] unless query_option['read_timeout'].blank?
+      post_params[:sparql_limit] = query_option['sparql_limit'] unless query_option['sparql_limit'].blank?
+      post_params[:answer_limit] = query_option['answer_limit'] unless query_option['answer_limit'].blank?
+      post_params[:cache] = query_option['cache'] unless query_option['cache'].blank?
+      post_params
     end
   end
 end
