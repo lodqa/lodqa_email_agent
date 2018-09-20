de764722c070af85ecaa99bc9a4a1d6cece8c07b
shsukh <39178089+shsukh@users.noreply.github.com>
2018-09-20 18:01:36 +0900

    DEV-116 startメールにオプションを表示する (#117)

--- app/mailers/start_mailer.rb
+++ app/mailers/start_mailer.rb
@@ -4,13 +4,14 @@
 class StartMailer < ActionMailer::Base
   default from: ENV['FROM_EMAIL']
 
-  def self.deliver_email(subject, to_email, query, search_id)
-    build_email(subject, to_email, query, search_id).deliver_now
+  def self.deliver_email(subject, to_email, query, search_id, options)
+    build_email(subject, to_email, query, search_id, options).deliver_now
   end
 
-  def build_email(subject, to_email, query, search_id)
+  def build_email(subject, to_email, query, search_id, options)
     @query = query
     @search_id = search_id
+    @options = options
     mail(to: to_email, subject: subject, &:text)
   end
 end
