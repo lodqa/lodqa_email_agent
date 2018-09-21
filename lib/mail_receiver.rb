38a358f83778a650472d041106567f3f36da85b4
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-09-21 18:24:59 +0900

    DEV-120 コメント指摘対応（メソッド名変更、「do end」を{}に変更）

--- lib/mail_receiver.rb
+++ lib/mail_receiver.rb
@@ -7,19 +7,16 @@ module MailReceiver
   class << self
     def receive_mail
       # 受信したメール取得
-      mails = pop_connection_mails
+      mails = pop_mails
       # 受信したメールが存在してない
       return [] if mails.empty?
-      mails.map do |mail|
-        # メールのハッシュ情報取得
-        mail_info(mail)
-      end
+      mails.map { |mail| to_hash(mail) }
     end
 
     private
 
     # pop接続で受信したメール取得
-    def pop_connection_mails
+    def pop_mails
       # popの接続
       Mail.defaults do
         retriever_method :pop3, address: Rails.configuration.pop_settings[:address],
@@ -37,7 +34,7 @@ module MailReceiver
     end
 
     # メールのハッシュ情報取得
-    def mail_info(mail)
+    def to_hash(mail)
       # 受信メール内容
       subject = mail.subject
       email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
