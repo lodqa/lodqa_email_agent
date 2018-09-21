ee87009b550d8f8778698abb7a23b135e1861a07
shsukh <39178089+shsukh@users.noreply.github.com>
2018-09-21 18:40:11 +0900

    DEV-120 MailReceiverのリファクタリング (#123)
    * DEV-120 MailReceiverのリファクタリング
    
    * DEV-120 コメント指摘対応（メソッド名変更、「do end」を{}に変更）
    
    * DEV-120 コメント指摘対応（メソッド名変更、コメント編集）

--- lib/mail_receiver.rb
+++ lib/mail_receiver.rb
@@ -6,6 +6,17 @@ require 'mail'
 module MailReceiver
   class << self
     def receive_mail
+      # メールを受信
+      mails = pop
+      # 受信したメールが0件のとき
+      return [] if mails.empty?
+      mails.map { |mail| to_hash(mail) }
+    end
+
+    private
+
+    # pop接続でメールを取得
+    def pop
       # popの接続
       Mail.defaults do
         retriever_method :pop3, address: Rails.configuration.pop_settings[:address],
@@ -19,19 +30,18 @@ module MailReceiver
              USER:#{Rails.configuration.pop_settings[:user_name]}/#{Rails.configuration.pop_settings[:password]}"
       mails = Mail.find_and_delete
       puts "#{mails.length} mails recieved."
-      return [] if mails.empty?
-
-      mails.map do |mail|
-        # 受信メール内容
-        subject = mail.subject
-        email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
-        query_option = body_option(mail.text_part.decoded)
-        # メールアドレスとタイトルとクエリーオプションをハッシュ化
-        { reply_to: email, query: subject, query_option: query_option }
-      end
+      mails
     end
 
-    private
+    # メールの情報をハッシュで返す
+    def to_hash(mail)
+      # 受信メール内容
+      subject = mail.subject
+      email = mail.reply_to ? mail.reply_to[0] : mail.from[0]
+      query_option = body_option(mail.text_part.decoded)
+      # メールアドレスとタイトルとクエリーオプションをハッシュ化
+      { reply_to: email, query: subject, query_option: query_option }
+    end
 
     # 本文情報でクエリのオプション値として設定
     def body_option(body)
