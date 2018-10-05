5ac0fde85347a0dfa22fb1b105bae121ffeee8fb
shsukh <39178089+shsukh@users.noreply.github.com>
2018-10-05 15:46:57 +0900

    DEV-136 CircleCIでRSpecを実行する (#138)
    * DEV-136 CircleCIでRSpecを実行する
    
    * DEV-136 lodqa_client.rbのrspec対応

--- app/labor/lodqa_client.rb
+++ app/labor/lodqa_client.rb
@@ -2,8 +2,8 @@
 
 # LODQA BSにHTTPリクエストを送る
 module LodqaClient
+  SERVER_URL = "http://#{ENV['HOST_LODQA_BS']}/searches"
   class << self
-    SERVER_URL = "http://#{ENV['HOST_LODQA_BS']}/searches"
     def post_query(question, address_to_send, option)
       return WarningMailer.deliver_email('warning mail', address_to_send) if question.blank?
       callback_url = "http://#{ENV['HOST_LODQA_EMAIL_AGENT']}/mail/#{address_to_send}/events"
@@ -13,8 +13,8 @@ module LodqaClient
       RestClient::Request.execute method: :post, url: SERVER_URL, payload: post_params
       puts 'POST succcess.'
     rescue Errno::ECONNREFUSED, Net::OpenTimeout, SocketError
-      puts 'POST failed.'
       FailureMailer.deliver_email('failure mail', address_to_send)
+      puts 'POST failed.'
     end
 
     private
