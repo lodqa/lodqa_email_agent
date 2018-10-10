061bb9d8a28cd6f04e4fdd487d6fb28a5a24c316
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-10-10 17:53:23 +0900

    DEV-135 送信メールの本文にURLの有効期限を追加する

--- app/mailers/finish_mailer.rb
+++ app/mailers/finish_mailer.rb
@@ -4,13 +4,14 @@
 class FinishMailer < ActionMailer::Base
   default from: ENV['FROM_EMAIL']
 
-  def self.deliver_email(subject, to_email, items_count, search_id)
-    build_email(subject, to_email, items_count, search_id).deliver_now
+  def self.deliver_email(subject, to_email, items_count, search_id, expiration_date)
+    build_email(subject, to_email, items_count, search_id, expiration_date).deliver_now
   end
 
-  def build_email(subject, to_email, items_count, search_id)
+  def build_email(subject, to_email, items_count, search_id, expiration_date)
     @items_count = items_count
     @search_id = search_id
+    @expiration_date = expiration_date.in_time_zone('Tokyo').strftime('%Y/%m/%d %H:%M')
     mail(to: to_email, subject: subject, &:text)
   end
 end
