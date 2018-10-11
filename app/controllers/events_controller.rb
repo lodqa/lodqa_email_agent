cf817e4eb6f5e258f5ec28d9493b870e14e97921
shsukh <39178089+shsukh@users.noreply.github.com>
2018-10-11 15:57:18 +0900

    DEV-135 送信メールの本文にURLの有効期限を追加する (#146)
    * DEV-135 送信メールの本文にURLの有効期限を追加する
    
    * DEV-135 コメント指摘対応（timezone編集）

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,10 +6,11 @@ class EventsController < ApplicationController
     # イベントの取得
     event = params[:event]
     return render status: 400 if event.blank?
-    # 送信先メールアドレス・検索ID・件名の取得
+    # 件名・送信先メールアドレス・検索ID・有効期限の取得
+    subject = "Re: #{URI.decode_www_form_component(params[:mail_subject_id])[0, 130]}"
     to_email = params[:mail_address_id]
     search_id = params[:search_id]
-    subject = "Re: #{URI.decode_www_form_component(params[:mail_subject_id])[0, 130]}"
+    expiration_date = params[:expiration_date]
     # イベントで判別し、開始・終了のメール送信を行う
     case event
     when 'start' then
@@ -18,14 +19,14 @@ class EventsController < ApplicationController
       # オプション情報の取得
       options = parse_options(params)
       # 開始メールを送信（SMTPサーバ使用）
-      StartMailer.deliver_email(subject, to_email, query, search_id, options)
+      StartMailer.deliver_email(subject, to_email, query, search_id, expiration_date, options)
     when 'finish' then
       # メール本文の取得
       answers = params[:answers]
       # 回答の項目数取得
       items_count = answers.blank? ? 0 : answers.length
       # 終了メールを送信（SMTPサーバ使用）
-      FinishMailer.deliver_email(subject, to_email, items_count, search_id)
+      FinishMailer.deliver_email(subject, to_email, items_count, search_id, expiration_date)
     else
       logger.info('イベント「'"#{event}"'」は想定していないイベントです。')
     end
--- app/mailers/start_mailer.rb
+++ app/mailers/start_mailer.rb
@@ -4,13 +4,14 @@
 class StartMailer < ActionMailer::Base
   default from: ENV['FROM_EMAIL']
 
-  def self.deliver_email(subject, to_email, query, search_id, options)
-    build_email(subject, to_email, query, search_id, options).deliver_now
+  def self.deliver_email(subject, to_email, query, search_id, expiration_date, options)
+    build_email(subject, to_email, query, search_id, expiration_date, options).deliver_now
   end
 
-  def build_email(subject, to_email, query, search_id, options)
+  def build_email(subject, to_email, query, search_id, expiration_date, options)
     @query = query
     @search_id = search_id
+    @expiration_date = expiration_date.in_time_zone('UTC').strftime('%Y/%m/%d %H:%M')
     @options = options
     mail(to: to_email, subject: subject, &:text)
   end
