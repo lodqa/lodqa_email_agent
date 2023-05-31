43226ef4d8bc39fcc06a1d38ce05533174afbad2
ledsun <shigeru.nakajima@gmail.com>
2023-05-31 16:38:30 +0900

    fix: Obtain from-address at runtime to ensure that tests do not fail

--- app/mailers/failure_mailer.rb
+++ app/mailers/failure_mailer.rb
@@ -2,13 +2,11 @@
 
 # 失敗メール送信
 class FailureMailer < ApplicationMailer
-  default from: ENV.fetch('FROM_EMAIL', nil)
-
   def self.deliver_email(subject, to_email)
     build_email(subject, to_email).deliver_now
   end
 
   def build_email(subject, to_email)
-    mail(to: to_email, subject:, &:text)
+    mail(from: ENV.fetch('FROM_EMAIL', nil), to: to_email, subject:, &:text)
   end
 end
